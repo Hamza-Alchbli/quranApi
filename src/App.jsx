60b1e9aab6af41d1a97af36dbacd676a0df24eda
Hamza <337118@student.mboutrecht.nl>
2023-06-16 20:10:16 +0200

    audio meege and calculate is semi complete

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
     const [elapsedSeconds, setElapsedSeconds] = useState(0);
     const [totalSeconds, setTotalSeconds] = useState(0);
     const [currentSurahAyahs, setCurrentSurahAyahs] = useState([]);
-
+    const audioContext = new AudioContext();
+    let sourceNode = null; // Keep track of the current audio source
     useEffect(() => {
         const fetchSurahs = async () => {
             try {
@@ -32,44 +33,81 @@ function App() {
 
     const generateSurahAudioURL = async (index) => {
         try {
-          const response = await fetch(
-            `https://api.alquran.cloud/v1/surah/${index + 1}/ar.alafasy`
-          );
-          const { data } = await response.json();
-          const surahData = data.ayahs;
-          const audioUrls = surahData.map((ayah) => ayah.audio);
-      
-          const audioContext = new AudioContext();
-          const audioBufferPromises = audioUrls.map((url) => {
-            return fetch(url)
-              .then((response) => response.arrayBuffer())
-              .then((arrayBuffer) =>
-                audioContext.decodeAudioData(arrayBuffer)
-              );
-          });
-      
-          const startTime = performance.now();
-      
-          const audioBuffers = await Promise.all(audioBufferPromises);
-          const totalDuration = audioBuffers.reduce(
-            (sum, buffer) => sum + buffer.duration,
-            0
-          );
-      
-          const endTime = performance.now();
-          const calculationTime = endTime - startTime;
-      
-          console.log("Total time (in seconds):", totalDuration);
-          console.log("Calculation time (in milliseconds):", calculationTime);
-          setLoading(false);
+            const response = await fetch(
+                `https://api.alquran.cloud/v1/surah/${index + 1}/ar.alafasy`
+            );
+            const { data } = await response.json();
+            const surahData = data.ayahs;
+            const audioUrls = surahData.map((ayah) => ayah.audio);
+
+            const audioBufferPromises = audioUrls.map((url) => {
+                return fetch(url)
+                    .then((response) => response.arrayBuffer())
+                    .then((arrayBuffer) =>
+                        audioContext.decodeAudioData(arrayBuffer)
+                    );
+            });
+
+            const audioBuffers = await Promise.all(audioBufferPromises);
+
+            const mergedBuffer = mergeAudioBuffers(audioBuffers);
+
+
+            if (sourceNode !== null) {
+                sourceNode.stop();
+                sourceNode.disconnect();
+            }
+            sourceNode = audioContext.createBufferSource();
+            sourceNode.buffer = mergedBuffer;
+
+            sourceNode.connect(audioContext.destination);
+            sourceNode.start();
+
+            setLoading(false);
+
+            const startTime = performance.now();
+            const totalDuration = audioBuffers.reduce(
+                (sum, buffer) => sum + buffer.duration,
+                0
+            );
+
+            const endTime = performance.now();
+            const calculationTime = endTime - startTime;
+
+            // console.log("Total time (in seconds):", totalDuration);
+            // console.log("Calculation time (in milliseconds):", calculationTime);
         } catch (err) {
-          console.log(err);
-          setError("Failed to fetch surah audio. Please try again later.");
-          setLoading(false);
+            console.log(err);
+            setError("Failed to fetch surah audio. Please try again later.");
+            setLoading(false);
         }
-      };
-      
-      
+    };
+
+    const mergeAudioBuffers = (audioBuffers) => {
+        const numberOfChannels = audioBuffers[0].numberOfChannels;
+        const mergedBufferLength = audioBuffers.reduce(
+            (length, buffer) => length + buffer.length,
+            0
+        );
+        const sampleRate = audioBuffers[0].sampleRate;
+        const mergedBuffer = audioContext.createBuffer(
+            numberOfChannels,
+            mergedBufferLength,
+            sampleRate
+        );
+
+        for (let channel = 0; channel < numberOfChannels; channel++) {
+            const channelData = mergedBuffer.getChannelData(channel);
+            let offset = 0;
+            for (const buffer of audioBuffers) {
+                const bufferData = buffer.getChannelData(channel);
+                channelData.set(bufferData, offset);
+                offset += bufferData.length;
+            }
+        }
+
+        return mergedBuffer;
+    };
 
     const playSurahAudio = () => {
         if (audioRef.current) {
