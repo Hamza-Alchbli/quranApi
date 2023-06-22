de78436b5f8909ba4c1f7cf527db61391220ffdd
Hamza <337118@student.mboutrecht.nl>
2023-06-22 17:12:57 +0200

    deleting vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./scss/main.scss";
-import { Analytics } from "@vercel/analytics/react";
-import { inject } from "@vercel/analytics";
 
-inject();
 ReactDOM.createRoot(document.getElementById("root")).render(
     // <React.StrictMode>
     <>
         <App />
-        <Analytics />
     </>
     // {/* </React.StrictMode>, */}
 );
