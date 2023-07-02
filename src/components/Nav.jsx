ae5c72540233bbcdcfb020d6c0400364e2581cca
Hamza <337118@student.mboutrecht.nl>
2023-07-02 21:33:14 +0200

    responsive nav with page tranistion animation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { faQuran } from "@fortawesome/free-solid-svg-icons";
+import { faQuran, faNavicon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Nav({ libraryStatus, setLibraryStatus }) {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
     return (
         <nav>
             <h1>
@@ -10,17 +18,44 @@ function Nav({ libraryStatus, setLibraryStatus }) {
                 <img src="/icons/waves.svg" alt="waves" />
                 Waves
             </h1>
-            <div>
+            <div className="nav-links">
                 <button onClick={() => setLibraryStatus(!libraryStatus)}>
                     Library
                     <FontAwesomeIcon icon={faQuran} size="lg" />
                 </button>
-                {/* <Link to="/">
-                    <button>Listen</button>
-                </Link>
-                <Link to="/read">
-                    <button>Read</button>
-                </Link> */}
+                <div
+                    className={`dropdown-container ${
+                        dropdownOpen ? "open" : ""
+                    }`}
+                >
+                    <button
+                        onClick={toggleDropdown}
+                        className="dropdown-button"
+                    >
+                        <FontAwesomeIcon icon={faNavicon} size="lg" />
+                    </button>
+
+                    <ul className="dropdown-content ">
+                        <Link
+                            to="/"
+                            onClick={() => {
+                                setLibraryStatus(false);
+                                toggleDropdown();
+                            }}
+                        >
+                            <li>Audio Steam</li>
+                        </Link>
+                        <Link
+                            to="/read"
+                            onClick={() => {
+                                setLibraryStatus(false);
+                                toggleDropdown();
+                            }}
+                        >
+                            <li>Reading Section</li>
+                        </Link>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
