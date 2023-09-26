741c01f74ebe3c00f6244959bf71a9eb801493d9
Hamza <337118@student.mboutrecht.nl>
2023-09-26 23:34:26 +0200

    p tag added to surah viewer

--- src/components/Surah.jsx
+++ src/components/Surah.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 function Surah({ currentSurah,reciter }) {
     return (
         <div className="surah-container">
+            <p>اللهم اجعل مثل ثواب ما سمعت للمرحوم الشهيد<br></br> أحمد عبد الكريم الرهبان</p>
             <img alt={reciter} src={`${reciter}.jpg`} />
             <h2>{currentSurah.name_arabic}</h2>
             <h3>{currentSurah.translated_name.name} || {currentSurah.name_simple}</h3>
